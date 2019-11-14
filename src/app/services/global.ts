01ddc1710283e1a8fff35792ccb933b90240f1c4
LuisAlavaWM <lalava@ecomundo.edu.ec>
2019-11-14 10:01:52 -0500

    implementacion de metodo refund de paymentez

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,3 +1,5 @@
 export const Global ={
-   url:'http://apiatrium.vespertinoecomundo.edu.ec/api/'
-};//produccion: http://apiatrium.vespertinoecomundo.edu.ec/api/  ----- desarrollo: http://192.168.0.195:3000/api/
+   url:'https://apiatrium.vespertinoecomundo.edu.ec/api/'
+};
+/* produccion: http://apiatrium.vespertinoecomundo.edu.ec/api/
+   desarrollo: http://192.168.0.195:3000/api/ */
