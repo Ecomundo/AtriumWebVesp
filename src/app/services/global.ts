cc1e9395b244247c11674fe80e25222ca5893882
LuisAlavaWM <lalava@ecomundo.edu.ec>
2020-03-09 11:12:24 -0500

    detalle boton pago

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export const Global ={
    url:'https://apiatrium.vespertinoecomundo.edu.ec/api/'
 };
-/* produccion: http://apiatrium.vespertinoecomundo.edu.ec/api/
+/* produccion: https://apiatrium.vespertinoecomundo.edu.ec/api/
    desarrollo: http://192.168.0.195:3000/api/ */
