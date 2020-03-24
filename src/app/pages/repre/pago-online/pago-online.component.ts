8c1f63f2639333c018f494dceb824360c62aafda
LuisAlavaWM <lalava@ecomundo.edu.ec>
2020-03-24 15:41:10 -0500

    correccion

--- src/app/pages/repre/pago-online/pago-online.component.ts
+++ src/app/pages/repre/pago-online/pago-online.component.ts
@@ -125,7 +125,7 @@ export class PagoOnlineComponent implements OnInit {
       <script>
       var paymentezCheckout = new PaymentezCheckout.modal({
           client_app_code: 'ECOMUNDO-EC-CLIENT',
-          client_app_key: 'nis24EWnymqMJ1Hn4UumxXJw4Kk38R'',
+          client_app_key: 'nis24EWnymqMJ1Hn4UumxXJw4Kk38R',
           locale: 'es',
           env_mode: 'stg',
           onOpen: function() {
